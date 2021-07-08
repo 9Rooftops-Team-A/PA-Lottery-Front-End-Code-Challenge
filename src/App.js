532f17107bb4b36ccbe63c5a67f83dacdf617f2f
Roy Wakumelo <roywakumelojr@gmail.com>
2021-07-07 20:15:03 -0500

    Added the app.js file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Components from './Component/index';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className='app-container'>
+      <h2 className='sidebar-title'>PICK 2</h2>
+      <Components/>
+    </div>
+  );
+}
+
+export default App;
