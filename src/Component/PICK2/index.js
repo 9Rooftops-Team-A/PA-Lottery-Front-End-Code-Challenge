ddfc3bbda610bfb0c7cdc470b638322ec665a905
Roy Wakumelo <roywakumelojr@gmail.com>
2021-07-07 20:23:01 -0500

    Main pick 2 component

---
+++ src/Component/PICK2/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Summary from './summary';
+import Content from './content';
+
+export default function index() {
+  return (
+    
+    <div>
+      <Summary/>
+      <Content/>
+    </div>
+  );
+}
