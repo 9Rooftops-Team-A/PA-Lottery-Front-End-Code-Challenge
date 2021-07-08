38b49df1fee35f522d09c5fe1684c802fd2e10ee
Roy Wakumelo <roywakumelojr@gmail.com>
2021-07-07 20:24:14 -0500

    Sidebar links component

---
+++ src/Component/SIDEBAR/sidebar-links.js
@@ -0,0 +1,118 @@
+import React from 'react';
+
+import './sidebar-links.scss';
+
+export default function SideBarLinks() {
+  return (
+    <div className='sidebar-container'>
+      <p className='pick-two-sidebar-link'>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-2.aspx"><strong>PICK 2</strong></a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-3.aspx">PICK 3</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-4.aspx">PICK 4</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-5.aspx">PICK 5</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Wild-Ball.aspx">WILD BALL</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Cash4Life.aspx">Cash4Life</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Cash-5.aspx">Cash 5</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Match-6.aspx">Match 6</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Mega-Millions.aspx">Mega Millions</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Powerball.aspx">Powerball</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Treasure-Hunt.aspx">Treasure Hunt</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Millionaire-Raffle.aspx">Millionaire Raffle</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Winning-Numbers-History.aspx">Winning Numbers History</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Pool-Play.aspx">Pool Play</a>
+      </p>
+      <p>
+        <a href="/About-PA-Lottery/How-to-Claim-Your-Prize.aspx">How to Claim Your
+          Prize</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Drawing-Chart-Desktop.aspx">Drawing Schedule</a>
+      </p>
+      <p>
+        <a href="https://www.palottery.state.pa.us/Draw-Games/Watch-Drawings.aspx">Watch Drawings</a>
+      </p>
+    </div>
+  );
+}
+
+<div className='sidebar-container'>
+  <p className='pick-two-sidebar-link'>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-2.aspx">PICK 2</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-3.aspx">PICK 3</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-4.aspx">PICK 4</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/PICK-5.aspx">PICK 5</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Wild-Ball.aspx">WILD BALL</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Cash4Life.aspx">Cash4Life</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Cash-5.aspx">Cash 5</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Match-6.aspx">Match 6</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Mega-Millions.aspx">Mega Millions</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Powerball.aspx">Powerball</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Treasure-Hunt.aspx">Treasure Hunt</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Millionaire-Raffle.aspx">Millionaire Raffle</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Winning-Numbers-History.aspx">Winning Numbers
+      History</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Pool-Play.aspx">Pool Play</a>
+  </p>
+  <p>
+    <a href="/About-PA-Lottery/How-to-Claim-Your-Prize.aspx">How to Claim Your
+      Prize</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Drawing-Chart-Desktop.aspx">Drawing Schedule</a>
+  </p>
+  <p>
+    <a href="https://www.palottery.state.pa.us/Draw-Games/Watch-Drawings.aspx">Watch Drawings</a>
+  </p>
+</div>
